4800c8f1f7dfa46baeb6f7dc98f860e434cd4b5d
JewgeniMartaler <eupogo@arcor.de>
2023-04-23 18:38:27 +0200

    zadanie 17.7.3

--- задание 17.7.3.py
+++ задание 17.7.3.py
@@ -1,7 +1,5 @@
 per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
 money  = int(input("введите сумму вклада:"))
-from decimal import Decimal
-
 deposit = []
 deposit.append(per_cent['ТКБ'] * money / 100)
 deposit.append(per_cent['СКБ'] * money / 100)
